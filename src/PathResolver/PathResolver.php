921ef7364934715bf56f3fe7e1ea5ad3c76dbda2
Stephan Vock <stephan.vock@gmail.com>
2018-04-19 15:31:19 +0200

    PathResolver: ignoring query params

--- src/PathResolver/PathResolver.php
+++ src/PathResolver/PathResolver.php
@@ -10,6 +10,7 @@ class PathResolver implements PathResolverInterface
 {
     public function getOpenApiPath(PathResolvableInterface $pathResolvable, SchemaInterface $schema): string
     {
+        $resolvablePath = parse_url($pathResolvable->getPath(), PHP_URL_PATH);
         $uriTemplate = new UriTemplate();
         $availablePaths = $schema->getAllPaths();
         foreach ($availablePaths as $path => $methods) {
@@ -17,19 +18,19 @@ class PathResolver implements PathResolverInterface
                 continue;
             }
 
-            if ($path === $pathResolvable->getPath()) {
+            if ($path === $resolvablePath) {
                 return $path;
             }
 
-            $params = $uriTemplate->extract($path, $pathResolvable->getPath());
+            $params = $uriTemplate->extract($path, $resolvablePath);
             $constructedPath = str_replace(array_map(function ($param) {
                 return '{' . $param . '}';
             }, array_keys($params)), array_values($params), $path);
-            if ($constructedPath === $pathResolvable->getPath()) {
+            if ($constructedPath === $resolvablePath) {
                 return $path;
             }
         }
 
-        throw new UnsupportedPathException(sprintf('Unable to resolve path: %s', $pathResolvable->getPath()));
+        throw new UnsupportedPathException(sprintf('Unable to resolve path: %s', $resolvablePath));
     }
 }
